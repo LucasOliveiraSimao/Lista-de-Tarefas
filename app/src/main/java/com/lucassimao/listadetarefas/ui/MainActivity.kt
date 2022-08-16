dc849accd7b8523853abc6545f83d23ffd6d2854
Lucas <oliveira.lucas20@gmail.com>
2022-08-15 21:44:31 -0400

    Moves package file

--- app/src/main/java/com/lucassimao/listadetarefas/ui/MainActivity.kt
+++ app/src/main/java/com/lucassimao/listadetarefas/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.lucassimao.listadetarefas
+package com.lucassimao.listadetarefas.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lucassimao.listadetarefas.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
