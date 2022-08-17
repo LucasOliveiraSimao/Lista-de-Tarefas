92efc533df2f27a7ca18fa418063d3565c4f9da0
Lucas <oliveira.lucas20@gmail.com>
2022-08-16 23:52:42 -0400

    Creates function to update note

--- app/src/main/java/com/lucassimao/listadetarefas/ui/fragments/UpdateNoteFragment.kt
+++ app/src/main/java/com/lucassimao/listadetarefas/ui/fragments/UpdateNoteFragment.kt
@@ -9,34 +9,35 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.lucassimao.listadetarefas.R
 import com.lucassimao.listadetarefas.data.model.NoteModel
-import com.lucassimao.listadetarefas.databinding.FragmentInsertNoteBinding
+import com.lucassimao.listadetarefas.databinding.FragmentUpdateNoteBinding
 import com.lucassimao.listadetarefas.ui.NoteViewModel
 import com.lucassimao.listadetarefas.utils.emptyFieldMessage
 import com.lucassimao.listadetarefas.utils.isEmptyNoteField
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class InsertNoteFragment : Fragment() {
-    private lateinit var binding: FragmentInsertNoteBinding
+class UpdateNoteFragment : Fragment() {
+    private lateinit var binding: FragmentUpdateNoteBinding
     private val viewModel by viewModel<NoteViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-
     ): View {
-        binding = FragmentInsertNoteBinding.inflate(inflater, container, false)
+        binding = FragmentUpdateNoteBinding.inflate(inflater, container, false)
 
-        binding.btnSaveNote.setOnClickListener {
-            if (!isEmptyNoteField(binding.etNoteTitle)) {
+        val bundle = arguments?.getParcelable<NoteModel>("key")
+        checkBundleNote(bundle)
+
+        binding.btnUpdateSaveNote.setOnClickListener {
+            if (!isEmptyNoteField(binding.etUpdateNoteTitle)) {
                 emptyFieldMessage(context, getString(R.string.title_field_cannot_be_empty)).show()
-            } else if (!isEmptyNoteField(binding.etNoteDesc)) {
+            } else if (!isEmptyNoteField(binding.etUpdateNoteDesc)) {
                 emptyFieldMessage(
                     context,
                     getString(R.string.description_field_cannot_be_empty)
                 ).show()
             } else {
-                val note = setupInsertNote(binding.etNoteTitle, binding.etNoteDesc)
-                viewModel.insertNote(note)
+                updateNote(bundle)
             }
 
             finishFragment()
@@ -44,20 +45,39 @@ class InsertNoteFragment : Fragment() {
         }
 
         return binding.root
+
+    }
+
+    private fun checkBundleNote(bundle: NoteModel?) {
+        if (bundle != null) {
+            binding.etUpdateNoteTitle.setText(bundle.note_title)
+            binding.etUpdateNoteDesc.setText(bundle.note_desc)
+        }
+    }
+
+    private fun updateNote(bundle: NoteModel?) {
+        if (bundle != null) {
+            val note = setupUpdateNote(
+                bundle.note_id,
+                binding.etUpdateNoteTitle,
+                binding.etUpdateNoteDesc
+            )
+            viewModel.updateNote(note)
+        }
     }
 
     private fun finishFragment() {
         findNavController().popBackStack()
     }
 
-    private fun setupInsertNote(
+    private fun setupUpdateNote(
+        noteID: Int,
         etNoteTitle: AppCompatEditText,
         etNoteDesc: AppCompatEditText
     ): NoteModel {
         val noteTile = etNoteTitle.text.toString()
         val noteDesc = etNoteDesc.text.toString()
-
-        return NoteModel(0, noteTile, noteDesc)
+        return NoteModel(noteID, noteTile, noteDesc)
     }
 
 }
