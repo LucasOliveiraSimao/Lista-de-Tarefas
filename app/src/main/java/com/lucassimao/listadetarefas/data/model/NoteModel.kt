e932056598845b7b07a8dd1bfb9c27bffa7154e5
Lucas <oliveira.lucas20@gmail.com>
2022-10-08 15:18:15 -0400

    Feat: Implements insert note in database

--- app/src/main/java/com/lucassimao/listadetarefas/data/model/NoteModel.kt
+++ app/src/main/java/com/lucassimao/listadetarefas/data/model/NoteModel.kt
@@ -2,6 +2,7 @@ package com.lucassimao.listadetarefas.data.model
 
 import android.os.Parcelable
 import androidx.recyclerview.widget.DiffUtil
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
@@ -12,8 +13,15 @@ data class NoteModel(
     @PrimaryKey(autoGenerate = true)
     var note_id: Int,
     val note_title: String,
-    val note_desc: String
+    val note_desc: String,
+    @ColumnInfo(name = "note_date", defaultValue = "")
+    val note_date: String,
+    @ColumnInfo(name = "note_hour", defaultValue = "")
+    val note_hour: String,
+    @ColumnInfo(name = "note_color", defaultValue = "0")
+    val note_color: Int
 ) : Parcelable {
+
     companion object : DiffUtil.ItemCallback<NoteModel>() {
         override fun areItemsTheSame(oldItem: NoteModel, newItem: NoteModel): Boolean {
             return oldItem.note_id == newItem.note_id
@@ -23,4 +31,5 @@ data class NoteModel(
             return oldItem == newItem
         }
     }
+
 }
--- app/src/main/java/com/lucassimao/listadetarefas/ui/fragments/InsertNoteFragment.kt
+++ app/src/main/java/com/lucassimao/listadetarefas/ui/fragments/InsertNoteFragment.kt
@@ -1,21 +1,35 @@
 package com.lucassimao.listadetarefas.ui.fragments
 
+import android.annotation.SuppressLint
+import android.icu.util.TimeZone
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
+import com.lucassimao.listadetarefas.R
 import com.lucassimao.listadetarefas.data.model.NoteModel
 import com.lucassimao.listadetarefas.databinding.FragmentInsertNoteBinding
 import com.lucassimao.listadetarefas.ui.NoteViewModel
+import com.lucassimao.listadetarefas.utils.emptyFieldMessage
+import com.lucassimao.listadetarefas.utils.format
+import com.lucassimao.listadetarefas.utils.isEmptyNoteField
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.util.*
 
 class InsertNoteFragment : Fragment() {
     private lateinit var binding: FragmentInsertNoteBinding
     private val viewModel by viewModel<NoteViewModel>()
+    private var colorNote: Int = 0
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,24 +37,119 @@ class InsertNoteFragment : Fragment() {
     ): View {
         binding = FragmentInsertNoteBinding.inflate(inflater, container, false)
 
-//        binding.btnSaveNote.setOnClickListener {
-//            if (!isEmptyNoteField(binding.etNoteTitle)) {
-//                emptyFieldMessage(context, getString(R.string.title_field_cannot_be_empty)).show()
-//            } else if (!isEmptyNoteField(binding.etNoteDesc)) {
-//                emptyFieldMessage(
-//                    context,
-//                    getString(R.string.description_field_cannot_be_empty)
-//                ).show()
-//            } else {
-//                val note = setupInsertNote(binding.etNoteTitle, binding.etNoteDesc)
-//                viewModel.insertNote(note)
-//            }
-//
-//            finishFragment()
-//
-//        }
+        insertListenersButtons()
+        selectedColorNote()
+
+        binding.btnSaveNote.setOnClickListener {
+
+            if (!isEmptyNoteField(binding.tilNoteTitle.editText)) {
+
+                emptyFieldMessage(context, getString(R.string.title_field_cannot_be_empty)).show()
+
+            } else if (!isEmptyNoteField(binding.tilNoteDesc.editText)) {
+
+                val message = getString(R.string.description_field_cannot_be_empty)
+                emptyFieldMessage(context, message).show()
+
+            } else {
+
+                binding.apply {
+
+                    val newNote = setupInsertNote(
+                        tilNoteTitle.editText,
+                        tilNoteDesc.editText,
+                        tilNoteDate.editText,
+                        tilNoteHour.editText
+                    )
+
+                    viewModel.insertNote(newNote)
+                }
+
+            }
+
+            finishFragment()
+        }
+
+        binding.btnCancelNote.setOnClickListener {
+            finishFragment()
+        }
 
         return binding.root
+    }
+
+    private fun selectedColorNote() {
+
+        binding.apply {
+            ivColorRed.setOnClickListener {
+                colorNote = context?.resources?.getColor(R.color.red_500)!!
+            }
+            ivColorPink.setOnClickListener {
+                colorNote = context?.resources?.getColor(R.color.pink_500)!!
+            }
+            ivColorDeepPurple.setOnClickListener {
+                colorNote = context?.resources?.getColor(R.color.deep_purple_500)!!
+            }
+            ivColorGreen.setOnClickListener {
+                colorNote = context?.resources?.getColor(R.color.green_500)!!
+            }
+            ivColorYellow.setOnClickListener {
+                colorNote = context?.resources?.getColor(R.color.yellow_500)!!
+            }
+            ivColorOrange.setOnClickListener {
+                colorNote = context?.resources?.getColor(R.color.orange_500)!!
+            }
+        }
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun insertListenersButtons() {
+
+        binding.apply {
+
+            tilNoteDate.editText?.setOnClickListener {
+                setupDate()
+            }
+
+            tilNoteHour.editText?.setOnClickListener {
+                setupHour()
+            }
+
+        }
+
+    }
+
+    private fun FragmentInsertNoteBinding.setupHour() {
+        val timePicker = MaterialTimePicker.Builder()
+            .setTimeFormat(TimeFormat.CLOCK_24H)
+            .build()
+
+        timePicker.addOnPositiveButtonClickListener {
+            val minutes =
+                if (timePicker.minute in 0..9) "0${timePicker.minute}" else timePicker.minute
+            val hour =
+                if (timePicker.hour in 0..9) "0${timePicker.hour}" else timePicker.hour
+
+            tilNoteHour.editText?.setText("$hour:$minutes")
+        }
+
+        timePicker.show(parentFragmentManager, null)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun FragmentInsertNoteBinding.setupDate() {
+        val datePicker = MaterialDatePicker.Builder.datePicker().build()
+
+        datePicker.addOnPositiveButtonClickListener {
+            val timeZone = TimeZone.getDefault()
+            val offset = timeZone.getOffset(Date().time * -1)
+
+            tilNoteDate.editText!!.setText(Date(it + offset).format())
+
+        }
+
+        datePicker.show(parentFragmentManager, "")
     }
 
     private fun finishFragment() {
@@ -48,13 +157,19 @@ class InsertNoteFragment : Fragment() {
     }
 
     private fun setupInsertNote(
-        etNoteTitle: EditText,
-        etNoteDesc: EditText
+        title: EditText?,
+        description: EditText?,
+        date: EditText?,
+        hour: EditText?
     ): NoteModel {
-        val noteTile = etNoteTitle.text.toString()
-        val noteDesc = etNoteDesc.text.toString()
 
-        return NoteModel(0, noteTile, noteDesc)
+        val noteTile = title?.text.toString()
+        val noteDesc = description?.text.toString()
+        val noteDate = date?.text.toString()
+        val noteHour = hour?.text.toString()
+
+        return NoteModel(0, noteTile, noteDesc, noteDate, noteHour, colorNote)
+
     }
 
 }
