e8f888397d8506b9780414031d4a7cdaee5ed1ce
Lucas <oliveira.lucas20@gmail.com>
2022-10-09 13:41:30 -0400

    Feat: Implements new update note screen

--- app/src/main/java/com/lucassimao/listadetarefas/utils/PopMenu.kt
+++ app/src/main/java/com/lucassimao/listadetarefas/utils/PopMenu.kt
@@ -5,12 +5,19 @@ import android.widget.PopupMenu
 import com.lucassimao.listadetarefas.R
 import com.lucassimao.listadetarefas.data.model.NoteModel
 
-fun showPopMenu(item: NoteModel, more: ImageView, deleteNote: (NoteModel) -> Unit) {
+fun showPopMenu(
+    item: NoteModel,
+    more: ImageView,
+    deleteNote: (NoteModel) -> Unit,
+    updateNote: (NoteModel) -> Unit
+) {
     val popupMenu = PopupMenu(more.context, more)
     popupMenu.menuInflater.inflate(R.menu.popup_menu, popupMenu.menu)
     popupMenu.setOnMenuItemClickListener {
         when (it.itemId) {
-            R.id.action_edit -> {}
+            R.id.action_edit -> {
+                updateNote(item)
+            }
             R.id.action_delete -> {
                 deleteNote(item)
             }
