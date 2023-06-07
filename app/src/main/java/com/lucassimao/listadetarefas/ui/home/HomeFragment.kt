766333a782c5f94b0e290d6d46d8614a1674c9cf
Lucas <oliveira.lucas20@gmail.com>
2023-06-07 00:01:56 -0400

    Creates a function that captures the click of the checkbox and saves in the database

--- app/src/main/java/com/lucassimao/listadetarefas/ui/home/HomeFragment.kt
+++ app/src/main/java/com/lucassimao/listadetarefas/ui/home/HomeFragment.kt
@@ -7,59 +7,68 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.lucassimao.listadetarefas.R
+import com.lucassimao.listadetarefas.data.model.TaskModel
 import com.lucassimao.listadetarefas.databinding.FragmentHomeBinding
-import com.lucassimao.listadetarefas.ui.NoteViewModel
+import com.lucassimao.listadetarefas.ui.TaskViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
-    private val viewModel by viewModel<NoteViewModel>()
-    private lateinit var adapter: NotesAdapter
+    private val viewModel by viewModel<TaskViewModel>()
+    private lateinit var adapter: TaskAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
-
         setupRecyclerView()
-
-        binding.fabInsertNote.setOnClickListener {
-            findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToInsertNoteFragment())
-        }
+        setupFabClickListener()
 
         return binding.root
     }
 
+    private fun setupFabClickListener() {
+        binding.fabInsertTask.setOnClickListener {
+            navigateToInsertTaskFragment()
+        }
+    }
+
+    private fun navigateToInsertTaskFragment() {
+        findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToInsertNoteFragment())
+    }
+
     private fun setupRecyclerView() {
-        adapter = NotesAdapter()
+        adapter = TaskAdapter(object : OnItemClickListener {
+            override fun onItemClick(task: TaskModel, isChecked: Boolean) {
+                val updateTask = TaskModel(task.id, task.name_task, isChecked)
+                viewModel.updateTask(updateTask)
+            }
+        })
+        binding.rvListTasks.adapter = adapter
+        observeTasks()
+        setupTaskActions()
+    }
 
-        deleteNote()
-        updateNote()
-
-        binding.rvListNotes.adapter = adapter
-        viewModel.allNotes.observe(viewLifecycleOwner) {
+    private fun observeTasks() {
+        viewModel.listAllTasks.observe(viewLifecycleOwner) {
             adapter.submitList(it)
         }
     }
 
-    private fun updateNote() {
-        adapter.updateNote = {
-            val bundle = Bundle()
-            bundle.putParcelable("key", it)
-
+    private fun setupTaskActions() {
+        adapter.updateTask = { task ->
+            val bundle = Bundle().apply {
+                putParcelable(KEY, task)
+            }
             findNavController().navigate(
-                R.id.action_homeFragment_to_updateNoteFragment, bundle
+                R.id.action_homeFragment_to_updateTaskFragment, bundle
             )
         }
 
-    }
-
-    private fun deleteNote() {
-        adapter.deleteNote = {
-            viewModel.deleteNote(it)
+        adapter.deleteTask = { task ->
+            viewModel.deleteTask(task)
         }
-
     }
 
     companion object {
