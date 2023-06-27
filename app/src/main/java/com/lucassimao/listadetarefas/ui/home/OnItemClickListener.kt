bf0918cee285cb98e1f4a8b05d240eeac409dfc6
Lucas <oliveira.lucas20@gmail.com>
2023-06-27 15:47:31 -0400

    Deletes unused file

--- app/src/main/java/com/lucassimao/listadetarefas/ui/home/OnItemClickListener.kt
+++
@@ -1,7 +0,0 @@
-package com.lucassimao.listadetarefas.ui.home
-
-import com.lucassimao.listadetarefas.data.model.TaskModel
-
-interface OnItemClickListener {
-    fun onItemClick(task: TaskModel, isChecked: Boolean)
-}
