bcf324dc936b03523bffe398a35c8f3092cc1a13
Lucas <oliveira.lucas20@gmail.com>
2023-06-27 16:16:04 -0400

    Refactors field validation

--- app/src/main/java/com/lucassimao/listadetarefas/ui/insert/InsertTaskFragment.kt
+++ app/src/main/java/com/lucassimao/listadetarefas/ui/insert/InsertTaskFragment.kt
@@ -4,26 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.lucassimao.listadetarefas.R
+import com.google.android.material.button.MaterialButton
 import com.lucassimao.listadetarefas.data.model.TaskModel
 import com.lucassimao.listadetarefas.databinding.FragmentInsertTaskBinding
 import com.lucassimao.listadetarefas.ui.TaskViewModel
-import com.lucassimao.listadetarefas.utils.emptyFieldMessage
-import com.lucassimao.listadetarefas.utils.isTaskFieldEmpty
+import com.lucassimao.listadetarefas.utils.enableButtonWhenEditTextHasText
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class InsertTaskFragment : Fragment() {
     private lateinit var binding: FragmentInsertTaskBinding
     private val viewModel by viewModel<TaskViewModel>()
-    private var message: String = ""
+    private var editTextNameTask: EditText? = null
+    private var buttonSaveTask: MaterialButton? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentInsertTaskBinding.inflate(inflater, container, false)
+        binding = FragmentInsertTaskBinding.inflate(layoutInflater)
 
         return binding.root
     }
@@ -31,8 +32,12 @@ class InsertTaskFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        editTextNameTask = binding.contentInsertTask.tilTaskTitle.editText
+        buttonSaveTask = binding.contentInsertTask.btnSaveTask
+
         setupSaveButton()
         setupCancelButton()
+        enableButtonWhenEditTextHasText(editTextNameTask, buttonSaveTask)
 
         binding.contentInsertTask.btnCancelTask.setOnClickListener {
             closeScreen()
@@ -40,26 +45,15 @@ class InsertTaskFragment : Fragment() {
     }
 
     private fun setupSaveButton() {
-        binding.contentInsertTask.btnSaveTask.setOnClickListener {
-            val editText = binding.contentInsertTask.tilTaskTitle.editText
+        buttonSaveTask!!.setOnClickListener {
+            val task = editTextNameTask!!.text.toString()
+            val newTask = TaskModel(0, task, false)
 
-            if (isTaskFieldEmpty(editText)) {
-                message = getString(R.string.task_field_cannot_be_empty)
-                showEmptyFieldMessage(message)
-            } else {
-                val task = editText?.text.toString()
-                val newTask = TaskModel(0, task, false)
+            viewModel.insertTask(newTask)
 
-                viewModel.insertTask(newTask)
-
-                closeScreen()
-            }
+            closeScreen()
 
         }
-    }
-
-    private fun showEmptyFieldMessage(message: String) {
-        emptyFieldMessage(requireContext(), message).show()
     }
 
     private fun setupCancelButton() {
--- app/src/main/java/com/lucassimao/listadetarefas/ui/update/UpdateTaskFragment.kt
+++ app/src/main/java/com/lucassimao/listadetarefas/ui/update/UpdateTaskFragment.kt
@@ -4,21 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.lucassimao.listadetarefas.R
+import com.google.android.material.button.MaterialButton
 import com.lucassimao.listadetarefas.data.model.TaskModel
 import com.lucassimao.listadetarefas.databinding.FragmentUpdateTaskBinding
 import com.lucassimao.listadetarefas.ui.TaskViewModel
 import com.lucassimao.listadetarefas.ui.home.HomeFragment
-import com.lucassimao.listadetarefas.utils.emptyFieldMessage
-import com.lucassimao.listadetarefas.utils.isTaskFieldEmpty
+import com.lucassimao.listadetarefas.utils.enableButtonWhenEditTextHasText
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class UpdateTaskFragment : Fragment() {
     private lateinit var binding: FragmentUpdateTaskBinding
     private val viewModel by viewModel<TaskViewModel>()
-    private var message: String = ""
+    private var editTextNameTask: EditText? = null
+    private var buttonUpdateTask: MaterialButton? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,29 +27,29 @@ class UpdateTaskFragment : Fragment() {
     ): View {
         binding = FragmentUpdateTaskBinding.inflate(inflater, container, false)
 
-        setupClickListeners()
-        checkBundleNote()
-
         return binding.root
 
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        editTextNameTask = binding.contentUpdateTask.tilTaskTitle.editText
+        buttonUpdateTask = binding.contentUpdateTask.btnSaveTask
+
+        setupClickListeners()
+        checkBundleNote()
+        enableButtonWhenEditTextHasText(editTextNameTask, buttonUpdateTask)
+
+    }
+
     private fun setupClickListeners() {
-        binding.contentUpdateTask.btnSaveTask.setOnClickListener {
-            val editText = binding.contentUpdateTask.tilTaskTitle.editText
-
-            if (isTaskFieldEmpty(editText)) {
-                message = getString(R.string.task_field_cannot_be_empty)
-                showEmptyFieldMessage(message)
-            } else {
-                updateTask()
-                closeScreen()
-            }
-
-            binding.contentUpdateTask.btnCancelTask.setOnClickListener {
-                closeScreen()
-            }
-
+        buttonUpdateTask!!.setOnClickListener {
+            updateTask()
+            closeScreen()
+        }
+        binding.contentUpdateTask.btnCancelTask.setOnClickListener {
+            closeScreen()
         }
     }
 
@@ -56,7 +57,7 @@ class UpdateTaskFragment : Fragment() {
         val bundle = arguments?.getParcelable<TaskModel>(HomeFragment.KEY)
 
         bundle?.let { task ->
-            binding.contentUpdateTask.tilTaskTitle.editText?.setText(task.name_task)
+            editTextNameTask!!.setText(task.name_task)
         }
     }
 
@@ -64,14 +65,10 @@ class UpdateTaskFragment : Fragment() {
         val bundle = arguments?.getParcelable<TaskModel>(HomeFragment.KEY)
 
         bundle?.let { task ->
-            val nameTask = binding.contentUpdateTask.tilTaskTitle.editText?.text.toString()
+            val nameTask = editTextNameTask!!.text.toString()
             val updatedTask = TaskModel(task.id, nameTask, task.check_task)
             viewModel.updateTask(updatedTask)
         }
-    }
-
-    private fun showEmptyFieldMessage(message: String) {
-        emptyFieldMessage(requireContext(), message).show()
     }
 
     private fun closeScreen() {
